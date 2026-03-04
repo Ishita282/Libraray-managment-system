407a628fd1cb0520c4c66a4e19a5a8e3cf106f38
Ishita282 <littleminiishita@gmail.com>
2026-03-04 22:06:31 +0530

    edit constructor

--- dtos/book-dto.js
+++ dtos/book-dto.js
@@ -5,7 +5,7 @@ class IssuedBook {
   year;
   genre;
   available;
-  issuedBy;
+  issuedBook;
   issuedDate;
   returnDate;
 
@@ -16,7 +16,7 @@ class IssuedBook {
     this.year = user.issuedBook.year;
     this.genre = user.issuedBook.genre;
     this.available = user.issuedBook.available;
-    this.issuedBy = user.issuedBy;
+    this.issuedBook = user.issuedBook;
     this.issuedDate = user.issuedDate;
     this.returnDate = user.returnDate;
   }
